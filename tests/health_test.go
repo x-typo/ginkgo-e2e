bc9bb63ce82a5cf8d101c18d14171ac2e99892b4
x-typo <87083014+x-typo@users.noreply.github.com>
2025-09-07 11:32:18 -0600

    rename tests for clarity

--- tests/health_test.go
+++ tests/health_test.go
@@ -1,37 +1,3 @@
-//package tests
-//
-//import (
-//	"net/http"
-//
-//	"github.com/go-resty/resty/v2"
-//	. "github.com/onsi/ginkgo/v2"
-//	. "github.com/onsi/gomega"
-//	"github.com/x-typ/ginkgo-e2e/internal/clients"
-//	"github.com/x-typ/ginkgo-e2e/internal/models/health"
-//	"github.com/x-typ/ginkgo-e2e/internal/services"
-//)
-//
-//var _ = Describe("Health Check", func() {
-//	var apiClient *resty.Client
-//
-//	BeforeEach(func() {
-//		apiClient = clients.NewApiClient()
-//	})
-//
-//	It("Return a 200 OK status with a success message", func() {
-//		resp, err := services.CheckHealth(apiClient)
-//
-//		Expect(err).NotTo(HaveOccurred())
-//		Expect(resp.StatusCode()).To(Equal(http.StatusOK))
-//
-//		By("verifying the response body payload", func() {
-//			responseBody := resp.Result().(*health.HealthResponse)
-//			Expect(responseBody.Success).To(BeTrue())
-//			Expect(responseBody.Message).To(Equal("Notes API is Running"))
-//		})
-//	})
-//})
-
 package tests
 
 import (
@@ -45,25 +11,23 @@ import (
 	"github.com/x-typ/ginkgo-e2e/internal/services"
 )
 
-var _ = Describe("Health Check API", func() {
+var _ = Describe("Health > ", func() {
 	var apiClient *resty.Client
 
 	BeforeEach(func() {
 		apiClient = clients.NewApiClient()
 	})
 
-	Context("when checking the /health-check endpoint", func() {
-		It("should return a 200 OK status with a success message", func() {
-			resp, err := services.CheckHealth(apiClient)
+	It("Return a 200 OK status with a success message", func() {
+		resp, err := services.CheckHealth(apiClient)
 
-			Expect(err).NotTo(HaveOccurred())
-			Expect(resp.StatusCode()).To(Equal(http.StatusOK))
+		Expect(err).NotTo(HaveOccurred())
+		Expect(resp.StatusCode()).To(Equal(http.StatusOK))
 
-			By("verifying the response body payload", func() {
-				responseBody := resp.Result().(*health.HealthResponse)
-				Expect(responseBody.Success).To(BeTrue())
-				Expect(responseBody.Message).To(Equal("Notes API is Running"))
-			})
+		By("verifying the response body payload", func() {
+			responseBody := resp.Result().(*health.HealthResponse)
+			Expect(responseBody.Success).To(BeTrue())
+			Expect(responseBody.Message).To(Equal("Notes API is Running"))
 		})
 	})
 })
