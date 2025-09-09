b92c5fb71518919c37f213e9dcb348ce553e29c6
x-typo <87083014+x-typo@users.noreply.github.com>
2025-09-09 17:29:18 -0600

    refactor to add BYs in user test

--- tests/users_test.go
+++ tests/users_test.go
@@ -30,47 +30,57 @@ var _ = Describe("User Authentication and Profile Endpoints -", func() {
 
 	Context("when logging in", func() {
 		It("should return a 200 OK status and a valid token with valid credentials", func() {
-			resp, err := services.LoginUser(apiClient, email, password)
+			By("making a login request to the API", func() {
+				resp, err := services.LoginUser(apiClient, email, password)
 
-			Expect(err).NotTo(HaveOccurred())
-			Expect(resp.StatusCode()).To(Equal(http.StatusOK))
+				Expect(err).NotTo(HaveOccurred())
+				Expect(resp.StatusCode()).To(Equal(http.StatusOK))
 
-			loginResponse := resp.Result().(*auth.LoginResponse)
+				loginResponse := resp.Result().(*auth.LoginResponse)
 
-			Expect(loginResponse.Success).To(BeTrue())
-			Expect(loginResponse.Message).To(Equal("Login successful"))
-			Expect(loginResponse.Data.Token).NotTo(BeEmpty())
-			Expect(loginResponse.Data.Email).To(Equal(email))
+				By("verifying the login response payload", func() {
+					Expect(loginResponse.Success).To(BeTrue())
+					Expect(loginResponse.Message).To(Equal("Login successful"))
+					Expect(loginResponse.Data.Token).NotTo(BeEmpty())
+					Expect(loginResponse.Data.Email).To(Equal(email))
+				})
+			})
 		})
 	})
 
 	Context("when authenticated", func() {
 		It("should successfully retrieve the user's profile information", func() {
-			loginResp, loginErr := services.LoginUser(apiClient, email, password)
-			Expect(loginErr).NotTo(HaveOccurred())
-			Expect(loginResp.StatusCode()).To(Equal(http.StatusOK), "Login failed, cannot proceed to get profile")
+			var token string
 
-			loginData := loginResp.Result().(*auth.LoginResponse)
-			token := loginData.Data.Token
-			Expect(token).NotTo(BeEmpty(), "Token was empty after a successful login")
+			By("authenticating the user to get a token", func() {
+				loginResp, loginErr := services.LoginUser(apiClient, email, password)
+				Expect(loginErr).NotTo(HaveOccurred())
+				Expect(loginResp.StatusCode()).To(Equal(http.StatusOK), "Login failed, cannot proceed to get profile")
 
-			GinkgoWriter.Println("Auth Token being used:", token)
+				loginData := loginResp.Result().(*auth.LoginResponse)
+				token = loginData.Data.Token
+				Expect(token).NotTo(BeEmpty(), "Token was empty after a successful login")
+			})
 
-			profileResp, profileErr := services.GetUserProfile(apiClient, token)
+			By("making a request to the user profile endpoint", func() {
+				GinkgoWriter.Println("Auth Token being used:", token)
+				profileResp, profileErr := services.GetUserProfile(apiClient, token)
 
-			Expect(profileErr).NotTo(HaveOccurred())
-			Expect(profileResp.StatusCode()).To(Equal(http.StatusOK))
+				Expect(profileErr).NotTo(HaveOccurred())
+				Expect(profileResp.StatusCode()).To(Equal(http.StatusOK))
 
-			profileResponse := profileResp.Result().(*auth.UserProfileInfoResponse)
+				profileResponse := profileResp.Result().(*auth.UserProfileInfoResponse)
 
-			Expect(profileResponse.Success).To(BeTrue())
-			Expect(profileResponse.Message).To(Equal("Profile successful"))
-			Expect(profileResponse.Data.ID).NotTo(BeEmpty())
-
-			Expect(profileResponse.Data.Name).To(Equal("Tyson"))
-			Expect(profileResponse.Data.Email).To(Equal(email))
-			Expect(profileResponse.Data.Phone).To(Equal("5556667777"))
-			Expect(profileResponse.Data.Company).To(Equal("Test Company"))
+				By("verifying the profile response payload", func() {
+					Expect(profileResponse.Success).To(BeTrue())
+					Expect(profileResponse.Message).To(Equal("Profile successful"))
+					Expect(profileResponse.Data.ID).NotTo(BeEmpty())
+					Expect(profileResponse.Data.Name).To(Equal("Tyson"))
+					Expect(profileResponse.Data.Email).To(Equal(email))
+					Expect(profileResponse.Data.Phone).To(Equal("5556667777"))
+					Expect(profileResponse.Data.Company).To(Equal("Test Company"))
+				})
+			})
 		})
 	})
 })
