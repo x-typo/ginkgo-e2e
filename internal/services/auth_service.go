c5cbcf39adc7d563d8ff77a32f58eae9a2611424
x-typo <87083014+x-typo@users.noreply.github.com>
2025-09-09 16:49:56 -0600

    change from login_service to auth_service

--- internal/services/auth_service.go
+++ internal/services/auth_service.go
@@ -2,22 +2,30 @@ package services
 
 import (
 	"github.com/go-resty/resty/v2"
-	"github.com/x-typ/ginkgo-e2e/internal/models/login"
+	"github.com/x-typ/ginkgo-e2e/internal/models/auth"
 	"github.com/x-typ/ginkgo-e2e/internal/routes"
 )
 
 func LoginUser(client *resty.Client, email string, password string) (*resty.Response, error) {
-	var loginResponse login.LoginResponse
+	var loginResponse auth.LoginResponse
 
 	requestBody := map[string]string{
 		"email":    email,
 		"password": password,
 	}
-
 	resp, err := client.R().
 		SetBody(requestBody).
 		SetResult(&loginResponse).
 		Post(routes.LoginEndpoint)
+	return resp, err
+}
 
+func GetUserProfile(client *resty.Client, token string) (*resty.Response, error) {
+	var userProfileResponse auth.UserProfileInfoResponse
+
+	resp, err := client.R().
+		SetHeader("x-auth-token", token).
+		SetResult(&userProfileResponse).
+		Get(routes.GetUserInfoEndpoint)
 	return resp, err
 }
