41e76d56b68045d74f3654e9fcad7367ad85f5cd
x-typo <87083014+x-typo@users.noreply.github.com>
2025-09-10 14:50:21 -0600

    add LogoutUser function to auth_service.go

--- internal/services/auth_service.go
+++ internal/services/auth_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"github.com/go-resty/resty/v2"
 	"github.com/x-typ/ginkgo-e2e/internal/models/auth"
+	"github.com/x-typ/ginkgo-e2e/internal/models/shared"
 	"github.com/x-typ/ginkgo-e2e/internal/routes"
 )
 
@@ -17,5 +18,15 @@ func LoginUser(client *resty.Client, email string, password string) (*resty.Resp
 		SetBody(requestBody).
 		SetResult(&loginResponse).
 		Post(routes.LoginEndpoint)
+	return resp, err
+}
+
+func LogoutUser(client *resty.Client, token string) (*resty.Response, error) {
+	var logoutResponse shared.BaseResponse
+
+	resp, err := client.R().
+		SetHeader("x-auth-token", token).
+		SetResult(&logoutResponse).
+		Delete(routes.LogoutEndpoint)
 	return resp, err
 }
